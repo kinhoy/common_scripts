4eebff9d7482961d9c76379e479475f41fce28b3
h9h <kinhoy97@gmail.com>
2023-10-09 16:53:07 +0800

    update

--- auto_rot_img.py
+++ auto_rot_img.py
@@ -2,63 +2,74 @@
 # 基于直线探测的文本类图片矫正算法
 import cv2
 import numpy as np
+import os
 
-def text_img_rotation_adjust_(img_path):
+def text_img_rotation_adjust(img_path, save_path):
+    print("working: {}".format(img_path))
     image = cv2.imread(img_path, cv2.IMREAD_COLOR)
     copy = image.copy()
+
+    # 开运算：先腐蚀，在膨胀
+    kernel = np.ones((32, 32), dtype=np.uint8)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel, 2)
+    # cv2.namedWindow("opening",0)
+    # cv2.imshow('opening', opening)
+
     canny = cv2.Canny(image,50,200,3)
+    # canny = cv2.Canny(image,300,700,3)
     lines = cv2.HoughLines(canny,1,np.pi/180,220)
-    # print(lines[0])
-    # print(lines.shape)
-    # for x1, y1, x2, y2 in lines[0]:
-    #     cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 100)
- 
     sum = 0.0
+    count = 0
     for i in range(0, len(lines)):
         rho, theta = lines[i][0][0], lines[i][0][1]
- 
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a * rho
         y0 = b * rho
-        x1 = int(x0 + 1000 * (-b))
-        y1 = int(y0 + 1000 * (a))
-        x2 = int(x0 - 1000 * (-b))
-        y2 = int(y0 - 1000 * (a))
- 
+        x1 = int(x0 + 10000 * (-b))
+        y1 = int(y0 + 10000 * (a))
+        x2 = int(x0 - 10000 * (-b))
+        y2 = int(y0 - 10000 * (a))
+        tmp_theta = theta / np.pi * 180
+        # 排除干扰直线
+        if tmp_theta < 80 or tmp_theta > 100:
+            continue
+        count = count + 1
         sum += theta
- 
+        # print(f"{theta} : {theta / np.pi * 180}")
         cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 1)
- 
-    average = sum/len(lines)
- 
+    # 计算平均旋转角度
+    if count == 0:
+        print("图片{}不太行").format(img_path)
+        return
+    average = sum / count
     print("average: {}".format(average))
     #度数转换
     angle = average / np.pi * 180
     print("angle: {}".format(angle))
     angle = angle-90
- 
-    # M = cv2.getRotationMatrix2D((image.shape[0]/2,image.shape[1]/2),angle,1)
-    # result = cv2.warpAffine(copy,M,(image.shape[1],image.shape[0]))
-    # cv2.imshow("warp image", result)
-    # cv2.imshow("canny",canny)
-    # cv2.imshow("lines", image)
-
     center = (image.shape[1] // 2, image.shape[0] // 2)
     rotMat = cv2.getRotationMatrix2D(center, angle, 1.0)
     # 用白色填充旋转后的图片周围
     cv_dst_img = cv2.warpAffine(copy, rotMat, (image.shape[1],image.shape[0]), borderValue=(255, 255, 255))
-    # cv2.imshow("cv_dst_img", cv_dst_img)
-    # cv2.imwrite('cv_dst_img.jpg',cv_dst_img)
-    cv2.imwrite('cv_dst_img2.jpg',cv_dst_img,[int(cv2.IMWRITE_JPEG_QUALITY),100])
+    cv2.imwrite(save_path, cv_dst_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
     # cv2.CV_IMWRITE_JPEG_QUALITY  设置图片格式为.jpeg或者.jpg的图片质量，其值为0---100（数值越大质量越高），默认95
     # cv2.CV_IMWRITE_WEBP_QUALITY  设置图片的格式为.webp格式的图片质量，值为0--100
     # cv2.CV_IMWRITE_PNG_COMPRESSION  设置.png格式的压缩比，其值为0--9（数值越大，压缩比越大），默认为3
 
-
 if __name__=='__main__':
-    # image = cv2.imread("aaaa.jpg",cv2.IMREAD_COLOR)
-    input = "source.jpg"
-    # input = "shuti.png" 竖向排版暂不考虑
-    text_img_rotation_adjust_(input)
-    cv2.waitKey(0)
+
+    input_path = r"D:\Documents\xpwszl14"
+    output_path = r"D:\Documents\xpwszl14_temp_rot"
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    for filename in os.listdir(input_path):
+        print(20*'*')
+        if filename.endswith(('.jpg', '.jpeg', '.png')):
+            input_img_path = os.path.join(input_path, filename)
+            output_img_path = os.path.join(output_path, filename)
+            text_img_rotation_adjust(input_img_path, output_img_path)
+
+    # 竖向排版暂不考虑
--- gaussian_rot_img.py
+++ gaussian_rot_img.py
@@ -2,33 +2,35 @@
 
 # 第一步 
 # 先把图像的边缘直线检测出来 得到threshold1和threshold2
-# import cv2 as cv
-# import numpy as np
+import cv2 as cv
+import numpy as np
 
-# src = cv.imread("aaaa.jpg")
-# gray_img = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+src = cv.imread("45.jpg")
+gray_img = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
 
-# def nothing(): 
-#     pass
+def nothing(): 
+    pass
 
-# cv.namedWindow("bar")
-# cv.createTrackbar("threshold1", "bar", 0, 255, nothing)
-# cv.createTrackbar("threshold2", "bar", 0, 255, nothing)
+cv.namedWindow("bar",0)
+cv.createTrackbar("threshold1", "bar", 0, 255, nothing)
+cv.createTrackbar("threshold2", "bar", 0, 255, nothing)
 
-# dst = cv.equalizeHist(gray_img)
-# # 高斯滤波降噪
-# gaussian = cv.GaussianBlur(dst, (9, 9), 0)
-# cv.imshow("gaussian", gaussian)
+dst = cv.equalizeHist(gray_img)
+# 高斯滤波降噪
+gaussian = cv.GaussianBlur(dst, (9, 9), 0)
+cv.namedWindow("gaussian",0)
+cv.imshow("gaussian", gaussian)
 
-# while True: 
-#     threshold1 = cv.getTrackbarPos("threshold1", "bar") 
-#     threshold2 = cv.getTrackbarPos("threshold2", "bar") 
-#     # 边缘检测 
-#     edges = cv.Canny(gaussian, threshold1, threshold2) 
-#     cv.imshow("edges", edges) 
-#     if cv.waitKey(1) & 0xFF == 27:
-#         break
-# cv.destroyAllWindows()
+while True: 
+    threshold1 = cv.getTrackbarPos("threshold1", "bar") 
+    threshold2 = cv.getTrackbarPos("threshold2", "bar") 
+    # 边缘检测 
+    edges = cv.Canny(gaussian, threshold1, threshold2) 
+    cv.namedWindow("edges",0)
+    cv.imshow("edges", edges) 
+    if cv.waitKey(1) & 0xFF == 27:
+        break
+cv.destroyAllWindows()
 
 # 上面程序调参得到参数
 # threshold1 = 201
@@ -36,60 +38,60 @@
 
 # 第二步 
 # 用上一步得到的threshold1和threshold2作为边缘检测Canny的参数
-import cv2 as cv
-import numpy as np
+# import cv2 as cv
+# import numpy as np
 
-src = cv.imread("aaaa.jpg")
-copy = src.copy()
-gray_img = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-dst = cv.equalizeHist(gray_img)
-# 高斯滤波降噪
-gaussian = cv.GaussianBlur(dst, (9, 9), 0)
-# cv.imshow("gaussian", gaussian)
+# src = cv.imread("35.jpg")
+# copy = src.copy()
+# gray_img = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+# dst = cv.equalizeHist(gray_img)
+# # 高斯滤波降噪
+# gaussian = cv.GaussianBlur(dst, (9, 9), 0)
+# # cv.imshow("gaussian", gaussian)
 
-# 边缘检测
-edges = cv.Canny(gaussian, 201, 255)
-cv.imshow("edges", edges)
-# Hough 直线检测
-# 重点注意第四个参数 阈值，只有累加后的值高于阈值时才被认为是一条直线，
-# 也可以把它看成能检测到的直线的最短长度（以像素点为单位）
-# 在霍夫空间理解为：至少有多少条正弦曲线交于一点才被认为是直线
-lines = cv.HoughLines(edges, 1.0, np.pi/180, 150)
+# # 边缘检测
+# edges = cv.Canny(gaussian, 231, 255)
+# cv.imshow("edges", edges)
+# # Hough 直线检测
+# # 重点注意第四个参数 阈值，只有累加后的值高于阈值时才被认为是一条直线，
+# # 也可以把它看成能检测到的直线的最短长度（以像素点为单位）
+# # 在霍夫空间理解为：至少有多少条正弦曲线交于一点才被认为是直线
+# lines = cv.HoughLines(edges, 1.0, np.pi/180, 150)
 
-# 将检测到的直线通过极坐标的方式画出来
-print(lines.ndim)
-print(lines.shape)
+# # 将检测到的直线通过极坐标的方式画出来
+# print(lines.ndim)
+# print(lines.shape)
 
-sum = 0.0
-for line in lines: 
-    # line[0]存储的是点到直线的极径和极角，其中极角是弧度表示的，theta是弧度 
-    rho, theta = line[0] 
-    # 下述代码为获取 (x0,y0) 具体值 
-    a = np.cos(theta) 
-    b = np.sin(theta) 
-    x0 = a*rho 
-    y0 = b*rho 
-    # 下图 1000 的目的是为了将线段延长 以 (x0,y0) 为基础，进行延长 
-    x1 = int(x0+1000*(-b)) 
-    y1 = int(y0+1000*a) 
-    x2 = int(x0-1000*(-b)) 
-    y2 = int(y0-1000*a) 
-    cv.line(src, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    sum += theta
+# sum = 0.0
+# for line in lines: 
+#     # line[0]存储的是点到直线的极径和极角，其中极角是弧度表示的，theta是弧度 
+#     rho, theta = line[0] 
+#     # 下述代码为获取 (x0,y0) 具体值 
+#     a = np.cos(theta) 
+#     b = np.sin(theta) 
+#     x0 = a*rho 
+#     y0 = b*rho 
+#     # 下图 1000 的目的是为了将线段延长 以 (x0,y0) 为基础，进行延长 
+#     x1 = int(x0+1000*(-b)) 
+#     y1 = int(y0+1000*a) 
+#     x2 = int(x0-1000*(-b)) 
+#     y2 = int(y0-1000*a) 
+#     cv.line(src, (x1, y1), (x2, y2), (0, 255, 0), 2)
+#     sum += theta
 
-average = sum / len(lines)
-print("average: {}".format(average))
-#度数转换
-angle = average / np.pi * 180
-print("angle: {}".format(angle))
-angle = angle-90
-center = (src.shape[1] // 2, src.shape[0] // 2)
-rotMat = cv.getRotationMatrix2D(center, angle, 1.0)
-# 用白色填充旋转后的图片周围
-cv_dst_img = cv.warpAffine(copy, rotMat, (src.shape[1],src.shape[0]), borderValue=(255, 255, 255))
-cv.imshow("rot", cv_dst_img)
-# cv2.imwrite('cv_dst_img2.jpg',cv_dst_img,[int(cv2.IMWRITE_JPEG_QUALITY),100])
+# average = sum / len(lines)
+# print("average: {}".format(average))
+# #度数转换
+# angle = average / np.pi * 180
+# print("angle: {}".format(angle))
+# angle = angle-90
+# center = (src.shape[1] // 2, src.shape[0] // 2)
+# rotMat = cv.getRotationMatrix2D(center, angle, 1.0)
+# # 用白色填充旋转后的图片周围
+# cv_dst_img = cv.warpAffine(copy, rotMat, (src.shape[1],src.shape[0]), borderValue=(255, 255, 255))
+# cv.imshow("rot", cv_dst_img)
+# # cv2.imwrite('cv_dst_img2.jpg',cv_dst_img,[int(cv2.IMWRITE_JPEG_QUALITY),100])
 
-cv.imshow("line", src)
-cv.waitKey()
-cv.destroyAllWindows()
+# cv.imshow("line", src)
+# cv.waitKey()
+# cv.destroyAllWindows()
